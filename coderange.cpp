d1fccdc9c95c8a313f4e2d5d2006ff206445cf40
Sanaa Elmosbah <80988796+elmossanaa@users.noreply.github.com>
2022-01-17 23:36:18 -0500

    Add files via upload

---
+++ coderange.cpp
@@ -0,0 +1,22 @@
+/**Write a program to test a number for being in the range 0 to 100, 
+inclusive*/
+#include <iostream>
+using namespace std;
+
+int main(){
+int n;
+cout<<"enter number:\n" <<endl;
+cin >>n;
+
+if(n>=0 && n<=100){
+cout <<"number in range (0 to 100)" <<endl;
+
+
+}
+else{
+    cout <<"number is not in the range of (0 to 100)" <<endl;
+    
+}
+
+
+}
